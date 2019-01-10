efaf81a6d823221ddc81cd7c535e42eb4c5b1637
Brad Schrag <brad.schrag@gmail.com>
2019-01-10 15:51:25 +0000

    Automatically backed up by Learn

---
+++ app/models/show.rb
@@ -0,0 +1,22 @@
+class Show < ActiveRecord::Base
+  
+  def self.highest_rating
+    self.maximum(:rating)
+  end
+  
+  def self.lowest_rating
+    self.minimum(:rating)
+  end
+  
+  def self.most_popular_show
+    self.where(:rating => highest_rating)[0]
+  end
+  
+  def self.least_popular_show
+    self.where(:rating => lowest_rating)[0]
+  end
+  
+  def self.ratings_sum
+    self.sum(:rating)
+  end
+end
